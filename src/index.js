b712be3a47e70904672e8d857934823c2311db0d
mihaipaulstd <mihaipaulstd@gmail.com>
2020-02-23 17:11:29 +0200

    initial socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
-
+io.on('connection', () => console.log('New user connected!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on localhost:${port}`));
+server.listen(port, () => console.log(`Listening on localhost:${port}`));
